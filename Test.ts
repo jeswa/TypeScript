b6845a7b22944d483593e3e155a7240b036eb9a6
jeswasolutions <crraji14@gmail.com>
2023-05-11 11:43:00 +0200

    Update Test.ts

--- Test.ts
+++ Test.ts
@@ -1,18 +1,30 @@
-class User {
+/ challenge.ts
+// Déclarer une interface User avec des propriétés typées
+interface User {
     name: string;
     age?: number;
     birthday?: string;
-
-    constructor(name: string, age?: number, birthday?: string) {
-        this.name = name;
-        if (age) {
-            this.age = age;
-        } else
-            if (birthday) {
-                let birthdayDate = new Date(birthday);
-                let ageInMilliSeconde = Date.now() - birthdayDate.getTime();
-                this.age = Math.floor(ageInMilliSeconde / 1000 / 60 / 60 / 24 / 365);
-            }
-    }
 }
+// Déclarer des types pour le paramètre et le retour de la fonction prettyPrintWilder
+const prettyPrintWilder = (users: Array<User>): void => {
+
+    users.map((user: User) => {
+        if ((user.age)) {
+            console.log(`${user.name} is ${user.age} years old`);
+        }
+        else {
+            console.log(`${user.name} is born on ${user.birthday}`); // Adapter user2 pour qu'il n'affiche plus d'erreur
+        }
+    });
+};
+
+// Typer wilders
+const wilders: User[] = [];
+const user1 = { name: "Pierre", age: 23 };
+const user2 = { name: "Paul", birthday: "10/02/1990" };
+const user3 = { name: "Jacques", age: 25 };
+wilders.push(user1);
+wilders.push(user2);
+wilders.push(user3);
+prettyPrintWilder(wilders);
 
